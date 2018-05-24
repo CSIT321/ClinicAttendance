1dfd7f62a04b4fbb609b700b20b76164966792b2
mcg343 <csit321ktw01a@proj-mac1.adeis.uow.edu.au>
2018-05-24 13:22:11 +1000

    Initial commit

---
+++ ClinicAttendance/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ClinicAttendance
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ClinicAttendance/Views/MainPage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ClinicAttendance
+{
+    public class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            Page calendarPage, tasksPage, infoPage = null;
+
+            switch (Device.RuntimePlatform)
+            {
+                // login screen
+                // calendar, tasks, info
+                case Device.iOS:
+                    calendarPage = new NavigationPage(new ItemsPage())
+                    {
+                        Title = "Calendar"
+                    };
+
+                    tasksPage = new NavigationPage(new AboutPage())
+                    {
+                        Title = "Tasks"
+                    };
+                    infoPage = new NavigationPage(new InfoPage())
+                    {
+                        Title = "Info"
+                    };
+                    calendarPage.Icon = "tab_feed.png";
+                    tasksPage.Icon = "tab_about.png";
+                    infoPage.Icon = "tab_about.png";
+               
+
+                    break;
+                default:
+                    calendarPage = new ItemsPage()
+                    {
+                        Title = "Calendar"
+                    };
+
+                    tasksPage =new AboutPage()
+                    {
+                        Title = "Tasks"
+                    };
+                    infoPage = new InfoPage()
+                    {
+                        Title = "Info"
+                    };
+
+                    //aboutPage = new AboutPage()
+                    //{
+                    //    Title = "About"
+                    //};
+                    break;
+            }
+
+            Children.Add(calendarPage);
+            Children.Add(tasksPage);
+            Children.Add(infoPage);
+
+            Title = Children[0].Title;
+        }
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+            Title = CurrentPage?.Title ?? string.Empty;
+        }
+    }
+}
